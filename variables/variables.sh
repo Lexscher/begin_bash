2cd9c1d25c1c1d5046affb7810f47d84395e4e64
Alexander Schelchere <ascellie@gmail.com>
2019-08-18 20:31:00 -0400

    variables

---
+++ variables/variables.sh
@@ -0,0 +1,11 @@
+#! /usr/local/bin/bash
+
+echo Shell: $BASH
+echo Version: $BASH_VERSION
+echo Home Directory: $HOME
+echo Current Working Directory: $PWD
+
+name=Alex
+age=23
+
+echo My name is $name and I am $age years old.
