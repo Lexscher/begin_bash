7b646df556bec89f4379a604dfa4b6ba84a6016e
Alexander Schelchere <ascellie@gmail.com>
2019-08-18 20:46:32 -0400

    user input: same line

--- practice/practice.sh
+++ practice/practice.sh
@@ -7,3 +7,10 @@ echo "Enter your name and email address: "
 read name email
 
 echo "$name, your email address is $email"
+
+read -p "What is your desired username? $current_user"
+echo $current_user
+echo
+read -sp "Enter your desired password: $password_digest"
+echo
+echo "your password has been saved."
