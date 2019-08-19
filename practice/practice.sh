f6c894ec139d14e9765efb86055607ea95487cc5
Alexander Schelchere <ascellie@gmail.com>
2019-08-18 21:07:19 -0400

    read and

--- practice/practice.sh
+++ practice/practice.sh
@@ -20,3 +20,7 @@ echo "What are your favorite foods? "
 read -a foods
 
 echo "I will order ${foods[0]}, ${foods[1]}, ${foods[2]} from Postmates."
+
+echo "What day is today?"
+read
+echo "Ahh I see... $REPLY is my favorite day of the week!"
