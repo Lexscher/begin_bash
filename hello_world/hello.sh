81fe4381d693ac69eac44c3cc53a118220bf988d
Alexander Schelchere <ascellie@gmail.com>
2019-08-18 20:25:28 -0400

    Variables

--- hello_world/hello.sh
+++ hello_world/hello.sh
@@ -1,3 +1,14 @@
 #! /usr/local/bin/bash
 
 echo "Hello World!"
+
+echo Shell: $BASH
+echo Version: $BASH_VERSION
+echo Home Directory: $HOME
+echo Current Working Directory: $PWD
+
+name=Alex
+age=23
+
+echo My name is $name and I am $age years old.
+
